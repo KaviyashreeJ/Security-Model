b9722644306fbbdbb1fc88fd3a15e4f9a1dce7ea
KaviyashreeJ <99129895+KaviyashreeJ@users.noreply.github.com>
2025-03-11 18:00:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,3 +55,20 @@ def predict():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    return jsonify({"message": "Prediction received", "data": data})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+@app.route('/')
+def home():
+    return "Flask API is running!"
+
